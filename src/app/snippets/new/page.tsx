b260c2801f531fcb0ebd80ff64c3d51819c177e1
grouls <roulstone.gary@gmail.com>
2024-03-14 18:27:58 +0000

    add in error handling and validation

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,26 +1,14 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+import { createSnippet } from "@/app/actions";
+import { useFormState } from "react-dom";
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    // server action
-    "use server";
-    // validate input
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-    // create new db record
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-    // redirect to root route
-    redirect("/");
-  }
+  const [formState, action] = useFormState(createSnippet, {
+    message: "",
+  });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -35,11 +23,16 @@ export default function SnippetCreatePage() {
           </label>
           <textarea className="primary-rounded w-full" name="code" />
         </div>
+        {formState.message && (
+          <div className="my-2 p-2 bg-red-200 border rounded border-red-400">
+            {formState.message}
+          </div>
+        )}
         <button
           className="primary-rounded main-cta hover:main-cta-hover"
           type="submit"
         >
-          Submit
+          Create
         </button>
       </div>
     </form>
